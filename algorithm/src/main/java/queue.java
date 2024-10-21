1f8cd2697af708f1ff7d5a592880725c603c4d12
now <nowing0108@kakao.com>
2024-10-21 23:52:42 +0900

    같은 숫자는 싫어

---
+++ algorithm/src/main/java/queue.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class queue {
+
+    public int[] solution(int[] arr) {
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        // 배열이 비어있지 않다면 첫 번째 숫자 추가
+        result.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                result.add(arr[i]);  // 중복되지 않은 숫자를 추가
+            }
+        }
+        
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+
+}
